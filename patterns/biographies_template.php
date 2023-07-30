709a45dd380ae7e749c1fa0fefc5d0e40172e0a5
Ahmed Abdou <hakam4all@yahoo.com>
2023-07-30 02:32:11 -0700

    bio page update

---
+++ patterns/biographies_template.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Title: biographies_template
+ * Slug: iff2023/biographies_template
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:template-part {"slug":"header-for-biographies"} /-->
+
+<!-- wp:group {"tagName":"main","align":"full","layout":{"type":"constrained"}} -->
+<main class="wp-block-group alignfull"><!-- wp:query {"queryId":0,"query":{"perPage":"12","pages":"0","offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":{"category":[7]}},"displayLayout":{"type":"flex","columns":5},"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-query alignwide"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"var:preset|spacing|30"},"blockGap":"var:preset|spacing|20","margin":{"top":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+<div class="wp-block-group" style="margin-top:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:var(--wp--preset--spacing--30)"><!-- wp:image {"id":157,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/icon_bio.png" alt="" class="wp-image-157"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"style":{"layout":{"selfStretch":"fit"},"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"backgroundColor":"pale-pink","textColor":"vivid-purple"} -->
+<h2 class="wp-block-heading has-vivid-purple-color has-pale-pink-background-color has-text-color has-background" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><strong>DIRECTORS</strong></h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:post-template -->
+<!-- wp:post-featured-image {"isLink":true,"width":"200px","height":"250px"} /-->
+
+<!-- wp:post-title {"textAlign":"center"} /-->
+<!-- /wp:post-template --></div>
+<!-- /wp:query -->
+
+<!-- wp:query {"queryId":0,"query":{"perPage":"12","pages":"0","offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":{"category":[8]}},"displayLayout":{"type":"flex","columns":5},"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-query alignwide"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"var:preset|spacing|30"},"blockGap":"var:preset|spacing|20","margin":{"top":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+<div class="wp-block-group" style="margin-top:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:var(--wp--preset--spacing--30)"><!-- wp:image {"id":157,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/icon_bio.png" alt="" class="wp-image-157"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"style":{"layout":{"selfStretch":"fit"},"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"backgroundColor":"pale-pink","textColor":"vivid-purple"} -->
+<h2 class="wp-block-heading has-vivid-purple-color has-pale-pink-background-color has-text-color has-background" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><strong>WRITERS</strong></h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:post-template -->
+<!-- wp:post-featured-image {"isLink":true,"width":"200px","height":"250px"} /-->
+
+<!-- wp:post-title {"textAlign":"center"} /-->
+<!-- /wp:post-template --></div>
+<!-- /wp:query -->
+
+<!-- wp:query {"queryId":0,"query":{"perPage":"12","pages":"0","offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":{"category":[9]}},"displayLayout":{"type":"flex","columns":5},"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-query alignwide"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"var:preset|spacing|30"},"blockGap":"var:preset|spacing|20","margin":{"top":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+<div class="wp-block-group" style="margin-top:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:var(--wp--preset--spacing--30)"><!-- wp:image {"id":157,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/icon_bio.png" alt="" class="wp-image-157"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"style":{"layout":{"selfStretch":"fit"},"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"backgroundColor":"pale-pink","textColor":"vivid-purple"} -->
+<h2 class="wp-block-heading has-vivid-purple-color has-pale-pink-background-color has-text-color has-background" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><strong>ACTORS</strong></h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:post-template -->
+<!-- wp:post-featured-image {"isLink":true,"width":"200px","height":"250px"} /-->
+
+<!-- wp:post-title {"textAlign":"center"} /-->
+<!-- /wp:post-template --></div>
+<!-- /wp:query --></main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer"} /-->
